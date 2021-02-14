00793c1f4d3608e8ad921689ab19041c1e7710af
Emilio Bondioli <e.bondioli@todo.to.it>
2021-02-14 18:23:28 +0100

    create autofarm component

---
+++ src/components/platforms/AutoFarm.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="autofarm-dashboard">
+    <h2 class="text-2xl font-bold text-center my-2 mt-8">Autofarm</h2>
+    <div class="totals flex justify-between items-center">
+      <p>
+        <label class="font-bold">Initial deposit: </label
+        ><span class="text-green">{{ fiat(totals.deposit, 2) }}</span>
+      </p>
+      <p>
+        <label class="font-bold">Current: </label
+        ><span class="text-green">{{ fiat(totals.total, 2) }}</span>
+      </p>
+      <p>
+        <label class="font-bold">Yield: </label
+        ><span class="text-green">{{ fiat(totals.yield, 2) }}</span>
+      </p>
+      <p>
+        <label class="font-bold">Rewards: </label
+        ><span class="text-green">{{ fiat(totals.rewards, 2) }}</span>
+      </p>
+      <VaultLabel
+        :text="fiat(totals.rewards + totals.yield, 2, true)"
+        label="Total: "
+        class="bg-blue-light text-white flex-row-reverse "
+      />
+    </div>
+    <h3 class="text-xl text-center">Single Asset</h3>
+    <div class="vaults flex">
+      <Vault :vault="vault" v-for="vault in vaults" :key="vault.id" />
+    </div>
+    <h3 class="text-xl text-center">Liquidity Pools</h3>
+    <div class="vaults-lp flex flex-wrap">
+      <Vault :vault="vault" v-for="vault in liquidityVaults" :key="vault.id" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vault from "@/components/Vault.vue";
+import VaultLabel from "@/components/VaultLabel.vue";
+import { ref } from "vue";
+import { useStore } from "vuex";
+import useFormats from "@/components/composables/use-formats.js";
+import useVault from "@/components/composables/use-vault.js";
+
+export default {
+  name: "AutoFarm",
+  components: {
+    Vault,
+    VaultLabel,
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    const store = useStore();
+    const { fiat, currency } = useFormats(store);
+
+    const vaults = ref(props.data.vaults.vaults);
+    const liquidityVaults = ref(props.data.LPVaults.vaults);
+    const totals = ref({
+      deposit:
+        props.data.vaults.totalUSDValues.deposit +
+        props.data.LPVaults.totalUSDValues.deposit,
+      yield:
+        props.data.vaults.totalUSDValues.yield +
+        props.data.LPVaults.totalUSDValues.yield,
+      total:
+        props.data.vaults.totalUSDValues.total +
+        props.data.LPVaults.totalUSDValues.total,
+      rewards: calculateTotalRewards(
+        props.data.vaults.vaults.concat(props.data.LPVaults.vaults)
+      ),
+    });
+
+    function calculateTotalRewards(vaults) {
+      return vaults
+        .map((v) => {
+          const { totalRewards, convertReward } = useVault(v);
+          return convertReward(totalRewards.value);
+        })
+        .reduce((a, b) => a + b);
+    }
+
+    return { vaults, liquidityVaults, fiat, currency, totals };
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
