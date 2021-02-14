025783e450a38d251853f6b924043b8494785af2
Emilio Bondioli <e.bondioli@todo.to.it>
2021-02-14 18:26:46 +0100

    update tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,46 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       green: {
-        light: '#6FFFE9',
-        DEFAULT: '#5BC0BE',
-        dark: '#3A506B',
+        light: "#6FFFE9",
+        DEFAULT: "#5BC0BE",
+        dark: "#3A506B",
+      },
+      blue: {
+        light: "#6FCBFF",
+        DEFAULT: "#5B83C0",
+        dark: "#5174AA",
       },
       red: {
-        light: '#FF6F92',
-        DEFAULT: '#C05B73',
-        dark: '#9F4C60',
+        light: "#FF6F92",
+        DEFAULT: "#C05B73",
+        dark: "#9F4C60",
       },
       white: colors.white,
       gray: {
-        darkest: '#1f2d3d',
-        dark: '#3c4858',
-        DEFAULT: '#1C2541',
-        light: '#e0e6ed',
-        lightest: '#f9fafc',
+        darkest: "#1f2d3d",
+        dark: "#3c4858",
+        DEFAULT: "#1C2541",
+        light: "#e0e6ed",
+        lightest: "#f9fafc",
       },
       black: {
-        DEFAULT: '#0B132B'
-      }
-    }
+        DEFAULT: "#0B132B",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      opacity: ["disabled"],
+      cursor: ["disabled"],
+    },
   },
   plugins: [],
-}
+};
