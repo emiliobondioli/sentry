33e056638445621ecda4291df2e492c461bd78ad
Emilio Bondioli <e.bondioli@todo.to.it>
2021-02-17 13:00:37 +0100

    fix enabled icon color

--- src/components/DarkModeSwitch.vue
+++ src/components/DarkModeSwitch.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="dark-mode-toggle p-2 bg-gray w-8 h-8 flex justify-center items-center rounded-full cursor-pointer dark:bg-green-light dark:text-gray"
+    class="dark-mode-toggle p-2 bg-gray w-8 h-8 flex justify-center items-center rounded-full cursor-pointer dark:bg-green-light dark:text-black"
     @click="toggle"
   >
     <img src="@/assets/icons/moon.svg" class="fill-current" svg-inline />
