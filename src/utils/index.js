ebb40c33b3d9a0789951daedebc9b3947fa1a3b3
Emilio Bondioli <e.bondioli@todo.to.it>
2021-02-17 01:06:00 +0100

    add utility to parse contract methods

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,28 @@
 export function intersection(arr1, arr2) {
-  return arr1.filter(el => arr2.includes(el))
+  return arr1.filter((el) => arr2.includes(el));
+}
+
+export function parseContractMethods(contract) {
+  const allMethods = Object.keys(contract.methods);
+  const methods = allMethods
+    .map((method) => {
+      const args = [];
+      if (!method.includes("0x") && !method.includes("(")) {
+        const shape = allMethods.find(
+          (m) => m.startsWith(method + "(") && m !== method + "()"
+        );
+        if (shape) {
+          const params = shape.split(",");
+          for (let i = 0; i < params.length; i++) {
+            args.push(0);
+          }
+        }
+        const signature = contract.methods[method](...args)._method.signature;
+        return { method, signature };
+      } else if (method.includes("0x")) {
+        return { method };
+      }
+    })
+    .filter((m) => m);
+  console.log(methods);
 }
