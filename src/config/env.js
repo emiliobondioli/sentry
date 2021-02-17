32f7817d445943384182d61977f14a4b58e519c9
Emilio Bondioli <e.bondioli@todo.to.it>
2021-02-17 23:04:02 +0100

    create common config files

---
+++ src/config/env.js
@@ -0,0 +1,3 @@
+export default {
+  localStoragePrefix: "sentry.farm-",
+};
---
+++ src/config/platforms.js
@@ -0,0 +1,9 @@
+export const autofarm = {
+  id: 0,
+  slug: "autofarm",
+  name: "autofarm",
+  url: "https://api.autofarm.network/bsc/get_farms_data",
+  address: "0x0895196562c7868c5be92459fae7f877ed450452",
+};
+
+export default [autofarm];
