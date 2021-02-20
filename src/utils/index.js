09d6906192221767f0023bd3e20b4676ed261bab
Emilio Bondioli <e.bondioli@todo.to.it>
2021-02-20 14:00:19 +0100

    use web3 methods for addresses and conversions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,39 +1,16 @@
+import web3 from "@/services/common/web3";
+
 export function intersection(arr1, arr2) {
   return arr1.filter((el) => arr2.includes(el));
 }
 
-export function parseContractMethods(contract) {
-  const allMethods = Object.keys(contract.methods);
-  const methods = allMethods
-    .map((method) => {
-      const args = [];
-      if (!method.includes("0x") && !method.includes("(")) {
-        const shape = allMethods.find(
-          (m) => m.startsWith(method + "(") && m !== method + "()"
-        );
-        if (shape) {
-          const params = shape.split(",");
-          for (let i = 0; i < params.length; i++) {
-            args.push(0);
-          }
-        }
-        const signature = contract.methods[method](...args)._method.signature;
-        return { method, signature };
-      } else if (method.includes("0x")) {
-        return { method };
-      }
-    })
-    .filter((m) => m);
-  console.log(methods);
-}
-
 export function convertValue(value) {
-  return Number(value) * 0.000000000000000001;
+  return web3.utils.fromWei(value);
 }
 
 export function parseAddress(address) {
   if (!address) return null;
-  return address.toLowerCase();
+  return web3.utils.toChecksumAddress(address);
 }
 
 export function merge(array, data) {
