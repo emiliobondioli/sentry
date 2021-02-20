eb66d31819ad36cbf845682770cd05be33a0a4b6
Emilio Bondioli <e.bondioli@todo.to.it>
2021-02-20 14:02:55 +0100

    correctly calculate total gain and rewards

--- src/components/composables/use-pool.js
+++ src/components/composables/use-pool.js
@@ -1,22 +1,25 @@
 import { computed } from "vue";
 
-export default function useVault(vault) {
+export default function usePool(pool) {
   const tokenYield = computed(
-    () => vault.currentTokens - vault.depositedTokens
+    () => pool.currentTokens - pool.depositedTokens
   );
 
-  const totalRewards = computed(() => vault.pendingRewards);
+  const totalRewards = computed(() => pool.pendingRewards);
 
-  const convert = (v) => v * vault.info.wantPrice;
-  const convertReward = (v) => v * vault.info.rewardPrice;
+  const convert = (v) => v * pool.info.wantPrice;
+  const convertReward = (v) => v * pool.info.rewardPrice;
 
   const tokenGain = computed(() => convert(tokenYield.value));
+  const totalGain = computed(() => convertReward(totalRewards.value) + tokenGain.value);
 
   return {
     convert,
     convertReward,
     tokenYield,
     totalRewards,
-    tokenGain,
+    totalGain,
+    tokenGain
   };
 }
+
