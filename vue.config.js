1079f01149ccbd365b68cfe8776609bb6e0812cc
Emilio Bondioli <e.bondioli@todo.to.it>
2021-02-22 13:51:41 +0100

    lint

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-	chainWebpack: config => {
-		config.module
-			.rule("vue")
-			.use("vue-svg-inline-loader")
-				.loader("vue-svg-inline-loader")	}
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-svg-inline-loader")
+      .loader("vue-svg-inline-loader");
+  },
 };
